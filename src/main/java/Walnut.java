6b928c62fe263b052cf70eb6edaebae6ea0c14d7
Ethan <Ethanjoshleong@gmail.com>
2026-08-20 14:00:52 +0800

    Added the ability to mark tasks as done and undone

--- src/main/java/Walnut.java
+++ src/main/java/Walnut.java
@@ -17,29 +17,45 @@ public class Walnut {
             + "____________________________________________________________";
         System.out.println(banner);
         System.out.println(greeting);
-        int exit = 0;
-        ArrayList<String> tasks = new ArrayList<>();
-        while (exit != 1) {
+        
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        while (true) {
             System.out.print("You: ");
             String input = System.console().readLine();
-            if (input.equals("list")) {
-                System.out.println("Walnut: Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + tasks.get(i));
-                }
-                System.out.println("____________________________________________________________");
-                continue;
-            } else {
-                tasks.add(input);
-            }
+            String[] words = input.split(" ");
+
             if (input.equals("bye")) {
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("Walnut: Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i).toString());
+                }
+                System.out.println("____________________________________________________________\n");
+                continue;
+            } else if (words[0].equals("mark")) {
+                System.out.println("____________________________________________________________\n"
+                + "Task marked as done!\n");
+                Task task = tasks.get(Integer.parseInt(words[1]) - 1);
+                task.markAsDone();
+                System.out.println(task.toString());
+                System.out.println("____________________________________________________________\n");
+            } else if (words[0].equals("unmark")) {
+                System.out.println("____________________________________________________________\n"
+                + "Task marked as not done!\n");
+                Task task = tasks.get(Integer.parseInt(words[1]) - 1);
+                task.markAsNotDone();
+                System.out.println(task.toString());
+                System.out.println("____________________________________________________________\n");
+            } else {
+                String output = "Walnut: Added task: "
+                + input
+                + "\n"
+                + "____________________________________________________________\n";
+                System.out.println(output); 
+                tasks.add(new Task(input));
             }
-            String output = "Walnut: Added task: "
-            + input
-            + "\n"
-            + "____________________________________________________________\n";
-            System.out.println(output);   
         }
         System.out.println(farewell);
     }
