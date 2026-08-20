60406bea7e67d5fc9a3d83e0878ca9416dc8c915
Ethan <Ethanjoshleong@gmail.com>
2026-08-20 11:58:01 +0800

    Changed chatbot name to Walnut and added a new banner

---
+++ src/main/java/Walnut.java
@@ -0,0 +1,10 @@
+public class Walnut {
+    public static void main(String[] args) {
+        String banner = " _    _       _             _\n"
+            + "| |  | | __ _| |_ __  _   _| |_\n"
+            + "| |/\\| |/ _` | | '_ \\| | | | __|\n"
+            + "|  /\\  | (_| | | | | | |_| | |_
+            + "|_/  \\_|\\__,_|_|_| |_|\\__,_|\\__|\n";
+        System.out.println(banner);
+    }
+}
