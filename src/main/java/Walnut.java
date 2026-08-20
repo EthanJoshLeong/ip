f01d3b11db0ac5daf6c49a24bff1390e84088919
Ethan <Ethanjoshleong@gmail.com>
2026-08-20 12:59:43 +0800

    Update the code to echo commands entered by the user, and exit when the user types the command bye

--- src/main/java/Walnut.java
+++ src/main/java/Walnut.java
@@ -5,12 +5,29 @@ public class Walnut {
             + "| |/\\| |/ _` | | '_ \\| | | | __|\n"
             + "|  /\\  | (_| | | | | | |_| | |_\n"
             + "|_/  \\_|\\__,_|_|_| |_|\\__,_|\\__|\n"
-            + "____________________________________________________________\n"
+            + "____________________________________________________________\n";
+        String greeting = "____________________________________________________________\n"
             + "Hello! I'm Walnut.\n"
             + "What can I do for you?\n"
-            + "____________________________________________________________\n"
+            + "____________________________________________________________\n";
+        String farewell = "____________________________________________________________\n"
             + "Bye. Hope to see you again soon!\n"
             + "____________________________________________________________";
         System.out.println(banner);
+        System.out.println(greeting);
+        int exit = 0;
+        while (exit != 1) {
+            System.out.print("You: ");
+            String input = System.console().readLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            String output = "Walnut: "
+            + input
+            + "\n"
+            + "____________________________________________________________";
+            System.out.println(output);   
+        }
+        System.out.println(farewell);
     }
 }
