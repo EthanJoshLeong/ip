0eb3b333f89e76acab958a5dd6f259d799d6b976
Ethan <Ethanjoshleong@gmail.com>
2026-08-20 13:30:34 +0800

    Added the ability to store whatever text the user enters and display it back when requested

--- src/main/java/Walnut.java
+++ src/main/java/Walnut.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Walnut {
     public static void main(String[] args) {
         String banner = " _    _       _             _\n"
@@ -16,16 +18,27 @@ public class Walnut {
         System.out.println(banner);
         System.out.println(greeting);
         int exit = 0;
+        ArrayList<String> tasks = new ArrayList<>();
         while (exit != 1) {
             System.out.print("You: ");
             String input = System.console().readLine();
+            if (input.equals("list")) {
+                System.out.println("Walnut: Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            } else {
+                tasks.add(input);
+            }
             if (input.equals("bye")) {
                 break;
             }
-            String output = "Walnut: "
+            String output = "Walnut: Added task: "
             + input
             + "\n"
-            + "____________________________________________________________";
+            + "____________________________________________________________\n";
             System.out.println(output);   
         }
         System.out.println(farewell);
