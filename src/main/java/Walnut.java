a79a5718e74c54cde76c51ad5bf891ebe33a527a
Ethan <Ethanjoshleong@gmail.com>
2026-08-27 17:33:56 +0800

    Add file loading and storage features

--- src/main/java/Walnut.java
+++ src/main/java/Walnut.java
@@ -1,4 +1,7 @@
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Walnut {
     public static void main(String[] args) {
@@ -19,12 +22,27 @@ public class Walnut {
         System.out.println(greeting);
         
         TaskList tasks = new TaskList();
+        Storage storage = new Storage();
+        List<String> data = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            data = storage.load();
+        } catch (IOException e) {
+            System.out.println("Error loading tasks.");
+        }
+
+        for (String line : data) {
+            Task task = Parser.parseTask(line);
+            if (task != null) {
+                tasks.add(task);
+            }
+        }
 
         while (true) {
             System.out.print("You: ");
-            String input = System.console().readLine();
+            String input = scanner.nextLine();
             String[] request = input.split(" ");
-            // String command = request[0];
 
             Commands command = Parser.parseCommand(request[0]);
 
@@ -75,6 +93,11 @@ public class Walnut {
                     + "\nTask marked as done!\n");
                     Task task = tasks.get(Integer.parseInt(request[1]) - 1);
                     task.markAsDone();
+                    try {
+                        storage.save(tasks);
+                    } catch (IOException e) {
+                        System.out.println("Error loading tasks.");
+                    }
                     System.out.println(task.toString());
                     System.out.println("\n____________________________________________________________\n");
                     break;
@@ -106,10 +129,16 @@ public class Walnut {
                     + "\nTask marked as not done!\n");
                     Task task = tasks.get(Integer.parseInt(request[1]) - 1);
                     task.markAsNotDone();
+                    try {
+                        storage.save(tasks);
+                    } catch (IOException e) {
+                        System.out.println("Error loading tasks.");
+                    }
                     System.out.println(task.toString());
                     System.out.println("____________________________________________________________\n");
                     break;
                 }
+
                 case TODO: {
                     String description = input.substring(4).trim();
                     if (description.isEmpty()) {
@@ -119,9 +148,15 @@ public class Walnut {
                     }
                     Task task = new ToDo(description);
                     tasks.add(task);
+                    try {
+                        storage.save(tasks);
+                    } catch (IOException e) {
+                        System.out.println("Error loading tasks.");
+                    }
                     System.out.println(tasks.addTaskToString(task));
                     break;
                 }
+
                 case EVENT: {
                     int fromIndex = input.indexOf("/from");
                     int toIndex = input.indexOf("/to");
@@ -140,9 +175,15 @@ public class Walnut {
                     String to = input.substring(toIndex + 3).trim();
                     Task task = new Events(description, from, to);
                     tasks.add(task);
+                    try {
+                        storage.save(tasks);
+                    } catch (IOException e) {
+                        System.out.println("Error loading tasks.");
+                    }
                     System.out.println(tasks.addTaskToString(task));
                     break;
                 }
+
                 case DEADLINE: {
                     int byIndex = input.indexOf("/by");
                     if (byIndex == -1) {
@@ -159,9 +200,15 @@ public class Walnut {
                     String by = input.substring(byIndex + 3).trim();
                     Task task = new Deadlines(description, by);
                     tasks.add(task);
+                    try {
+                        storage.save(tasks);
+                    } catch (IOException e) {
+                        System.out.println("Error loading tasks.");
+                    }
                     System.out.println(tasks.addTaskToString(task));
                     break;
                 }
+
                 case REMOVE: {
                     if (tasks.isEmpty()) {
                         System.out.println("Your task list is empty. Please add a task first.\n");
@@ -189,168 +236,17 @@ public class Walnut {
                     int index = Integer.parseInt(request[1]) - 1;
                     Task task = tasks.get(index);
                     tasks.remove(index);
+                    try {
+                        storage.save(tasks);
+                    } catch (IOException e) {
+                        System.out.println("Error loading tasks.");
+                    }
                     System.out.println(task.toString());
                     System.out.println("You have " + (tasks.size()) + " task[s] in your list.");
                     System.out.println("____________________________________________________________\n");
                     break;
                 }
             }
-    //         if (input.equals("bye")) {
-    //             break;
-                
-    //         } else if (input.equals("list")) {
-    //             System.out.println("Walnut: Here are the tasks in your list:");
-    //             System.out.println(tasks.toString());
-    //             System.out.println("____________________________________________________________\n");
-    //             continue;
-
-    //         } else if (command.equals("mark")) {
-    //             if (tasks.isEmpty()) {
-    //                 System.out.println("Your task list is empty. Please add a task first.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             try {
-    //                 int index = Integer.parseInt(request[1]) - 1;
-    //                 if (index < 0 || index >= tasks.size()) {
-    //                     System.out.println("Invalid task number. Please enter a number between 1 and " + tasks.size() + ".\n");
-    //                     System.out.println("____________________________________________________________\n");
-    //                     continue;
-    //                 }
-    //             } catch (NumberFormatException e) {
-    //                 System.out.println("Invalid task number. Please enter a valid number.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             } catch (ArrayIndexOutOfBoundsException e) {
-    //                 System.out.println("Please specify the task number to mark.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             System.out.println("____________________________________________________________\n"
-    //             + "\nTask marked as done!\n");
-    //             Task task = tasks.get(Integer.parseInt(request[1]) - 1);
-    //             task.markAsDone();
-    //             System.out.println(task.toString());
-    //             System.out.println("\n____________________________________________________________\n");
-                
-
-    //         } else if (command.equals("unmark")) {
-    //             if (tasks.isEmpty()) {
-    //                 System.out.println("Your task list is empty. Please add a task first.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             try {
-    //                 int index = Integer.parseInt(request[1]) - 1;
-    //                 if (index < 0 || index >= tasks.size()) {
-    //                     System.out.println("Invalid task number. Please enter a number between 1 and " + tasks.size() + ".\n");
-    //                     System.out.println("____________________________________________________________\n");
-    //                     continue;
-    //                 }
-    //             } catch (NumberFormatException e) {
-    //                 System.out.println("Invalid task number. Please enter a valid number.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             } catch (ArrayIndexOutOfBoundsException e) {
-    //                 System.out.println("Please specify the task number to mark.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             System.out.println("____________________________________________________________\n"
-    //             + "\nTask marked as not done!\n");
-    //             Task task = tasks.get(Integer.parseInt(request[1]) - 1);
-    //             task.markAsNotDone();
-    //             System.out.println(task.toString());
-    //             System.out.println("____________________________________________________________\n");
-
-    //         } else if (command.equals("todo")) {
-    //             String description = input.substring(4).trim();
-    //             if (description.isEmpty()) {
-    //                 System.out.println("The description of a todo cannot be empty.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             Task task = new ToDo(description);
-    //             tasks.add(task);
-    //             System.out.println(tasks.addTaskToString(task));
-
-    //         } else if (command.equals("event")) {
-    //             int fromIndex = input.indexOf("/from");
-    //             int toIndex = input.indexOf("/to");
-    //             if (fromIndex == -1 || toIndex == -1) {
-    //                 System.out.println("Invalid event format. Please use the format: event <description> /from <start time> /to <end time>\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             String description = input.substring(5, fromIndex).trim();
-    //             if (description.isEmpty()) {
-    //                 System.out.println("The description of an event cannot be empty.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             String from = input.substring(fromIndex + 5, toIndex).trim();
-    //             String to = input.substring(toIndex + 3).trim();
-    //             Task task = new Events(description, from, to);
-    //             tasks.add(task);
-    //             System.out.println(tasks.addTaskToString(task));
-
-    //         } else if (command.equals("deadline")) {
-    //             int byIndex = input.indexOf("/by");
-    //             if (byIndex == -1) {
-    //                 System.out.println("Invalid deadline format. Please use the format: deadline <description> /by <deadline>\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             String description = input.substring(9, byIndex).trim();
-    //             if (description.isEmpty()) {
-    //                 System.out.println("The description of a deadline cannot be empty.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             String by = input.substring(byIndex + 3).trim();
-    //             Task task = new Deadlines(description, by);
-    //             tasks.add(task);
-    //             System.out.println(tasks.addTaskToString(task)); 
-
-    //         } else if (command.equals("remove")) {
-    //             if (tasks.isEmpty()) {
-    //                 System.out.println("Your task list is empty. Please add a task first.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             try {
-    //                 int index = Integer.parseInt(request[1]) - 1;
-    //                 if (index < 0 || index >= tasks.size()) {
-    //                     System.out.println("Invalid task number. Please enter a number between 1 and " + tasks.size() + ".\n");
-    //                     System.out.println("____________________________________________________________\n");
-    //                     continue;
-    //                 }
-    //             } catch (NumberFormatException e) {
-    //                 System.out.println("Invalid task number. Please enter a valid number.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             } catch (ArrayIndexOutOfBoundsException e) {
-    //                 System.out.println("Please specify the task number to mark.\n");
-    //                 System.out.println("____________________________________________________________\n");
-    //                 continue;
-    //             }
-    //             System.out.println("____________________________________________________________\n"
-    //             + "\nTask removed!: \n");
-    //             int index = Integer.parseInt(request[1]) - 1;
-    //             Task task = tasks.get(index);
-    //             tasks.remove(index);
-    //             System.out.println(task.toString());
-    //             System.out.println("You have " + (tasks.size()) + " task[s] in your list.");
-    //             System.out.println("____________________________________________________________\n");
-    //         } else {
-    //             String output = "Invalid task format. Please use the format: todo <description> or event <description> /from <start time> /to <end time> or deadline <description> /by <deadline>"
-    //             + "\n"
-    //             + "____________________________________________________________\n";
-    //             System.out.println(output);
-    //         }
-    //     }
-    //     System.out.println(farewell);
-    // }
-        }       
+        }
     }
 }
