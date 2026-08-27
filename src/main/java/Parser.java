c11660b01f89c62aa23bdd8159b99ec328903f83
Ethan <Ethanjoshleong@gmail.com>
2026-08-27 17:30:05 +0800

    Add parseTask method for loading tasks

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Parser {
 
     public static Commands parseCommand(String command) {
@@ -30,5 +32,27 @@ public class Parser {
         default:
             return null;
         }
+    }
+
+    public static Task parseTask(String task) {
+        String[] data = task.split(" \\| ");
+        Task newTask;
+
+        if (data[0].equals("T")) {
+            newTask = new ToDo(data[2]);
+        } else if (data[0].equals("D")) {
+            newTask = new Deadlines(data[2], data[3]);
+        } else if (data[0].equals("E")) {
+            String[] dateTime = data[3].split("-");
+            newTask = new Events(data[2], dateTime[0], dateTime[1]);
+        } else {
+            return null;
+        }
+
+        if (data[1].equals("1")) {
+            newTask.markAsDone();
+        }
+
+        return newTask;
     }
 }
