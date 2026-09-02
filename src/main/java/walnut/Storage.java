dff6b30c0aab2f00b6ac891bd86fccc3aa6038a5
Ethan <Ethanjoshleong@gmail.com>
2026-09-02 12:31:47 +0800

    Fix Storage checkstyle issues

--- src/main/java/walnut/Storage.java
+++ src/main/java/walnut/Storage.java
@@ -11,8 +11,16 @@ import java.util.List;
  * Loads tasks from and saves tasks to Walnut's data file.
  */
 public class Storage {
-    Path path = Paths.get("./data/Walnut.txt");
-    Path dir = Paths.get("./data/");
+    private Path path = Paths.get("./data/Walnut.txt");
+    private Path dir = Paths.get("./data/");
+
+    private Path getPath() {
+        return path;
+    }
+
+    private Path getDir() {
+        return dir;
+    }
 
     /**
      * Returns all stored task records.
@@ -23,14 +31,14 @@ public class Storage {
      * @throws IOException If the data directory or file cannot be accessed.
      */
     public List<String> load() throws IOException {
-        if (Files.notExists(dir)) {
-            Files.createDirectories(dir);
+        if (Files.notExists(getDir())) {
+            Files.createDirectories(getDir());
         }
-        if (Files.notExists(path)) {
-            Files.createFile(path);
+        if (Files.notExists(getPath())) {
+            Files.createFile(getPath());
         }
         List<String> data;
-        data = Files.readAllLines(path);
+        data = Files.readAllLines(getPath());
         return data;
     }
 
@@ -41,11 +49,11 @@ public class Storage {
      * @throws IOException If the data directory or file cannot be accessed.
      */
     public void save(TaskList tasks) throws IOException {
-        if (Files.notExists(dir)) {
-            Files.createDirectories(dir);
+        if (Files.notExists(getDir())) {
+            Files.createDirectories(getDir());
         }
-        if (Files.notExists(path)) {
-            Files.createFile(path);
+        if (Files.notExists(getPath())) {
+            Files.createFile(getPath());
         }
 
         List<String> data = new ArrayList<>();
@@ -53,18 +61,18 @@ public class Storage {
         for (Task task : tasks) {
             String line = "";
             if (task instanceof ToDo) {
-                line = "T | " + (task.isDone() ? 1 : 0) +" | " +
-                        task.getDescription();
+                line = "T | " + (task.isDone() ? 1 : 0) + " | "
+                        + task.getDescription();
             } else if (task instanceof Deadline) {
-                line = "D | " + (task.isDone() ? 1 : 0) +" | " +
-                        task.getDescription() + " | " + ((Deadline) task).getDeadline();
+                line = "D | " + (task.isDone() ? 1 : 0) + " | "
+                        + task.getDescription() + " | " + ((Deadline) task).getDeadline();
             } else if (task instanceof Event) {
-                line = "E | " + (task.isDone() ? 1 : 0) +" | " +
-                        task.getDescription() + " | " + ((Event) task).getEventStartTime() +
-                        "-" + ((Event) task).getEventEndTime();
+                line = "E | " + (task.isDone() ? 1 : 0) + " | "
+                        + task.getDescription() + " | " + ((Event) task).getEventStartTime()
+                        + "-" + ((Event) task).getEventEndTime();
             }
             data.add(line);
         }
-        Files.write(path, data);
+        Files.write(getPath(), data);
     }
 }
